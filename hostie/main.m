d28ca4293eda6b35546b0d432b0ec5c030a5704e
Oliver Hirst <Ollie@MacBook.local>
2012-11-20 18:21:47 +0000

    Initial Commit

---
+++ hostie/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hostie
+//
+//  Created by Oliver Hirst on 20/11/2012.
+//  Copyright (c) 2012 TwentyFathoms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
